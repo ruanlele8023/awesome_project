40cba2172ec1eae0298e2adba42ff149fdc2eef2
ruanlele8023 <32916926+ruanlele8023@users.noreply.github.com>
2024-03-07 20:37:16 +0800

    feat: 修改配置文件路径

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,7 @@ mod util;
 
 #[tokio::main]
 async fn main() {
-    // let app = Router::new().route("/", get(handler));
-    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    // // run it with hyper on localhost:3000
-    // axum::Server::bind(&addr)
-    //     .serve(app.into_make_service())
-    //     .await
-    //     .unwrap();
-    let config_str = String::from("/Users/bytedance/RustroverProjects/awesome_project/src/config.toml");
+    let config_str = String::from("ABC");
     cfg::init(&config_str);
     db::init(cfg::config()).await;
     config::serve::serve().await;
